8ceeebc252ea1e74f3cbbda23abb85ba5651b4b8
xiangxinrong <xiangxinrong126@126.com>
2023-03-03 17:49:29 +0800

    Add index page

--- app.py
+++ app.py
@@ -18,15 +18,15 @@
 # 3.获取hello()函数的返回值，处理后返回给客户端（浏览器）
 # 4.浏览器接受响应，将其显示在窗口上
 
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -35,8 +35,22 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     print(url_for('hello'))
-    print(url_for('user_page',name='xingu'))
+    print(url_for('user_page',name='Daniel'))
     print(url_for('user_page',name='Peter'))
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'Test page'
+
+name = 'Aimi'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
